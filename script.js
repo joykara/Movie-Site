851c7ebd37d8e2e7b13a345a17d23745deb83fee
joykara <joykarani1@gmail.com>
2023-04-05 09:58:56 +0300

    fetch movies from API

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const API_LINK = "https://api.themoviedb.org/3/movie/550?api_key=6e7b2cbc9fa2d51988471aaf765f0237&page=1"
 const imgPath = ""
 const searchAPI = "https://api.themoviedb.org/3/movie/550?api_key=6e7b2cbc9fa2d51988471aaf765f0237&query="
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+function returnMovies(url) {
+    fetch(url).then[res => res.json()]
+        .then(function (data) {
+            console.log(data.results);
+            data.results.forEach(element => {
+                const div = document.createElement("div");
+            });
+        
+    })
+}
