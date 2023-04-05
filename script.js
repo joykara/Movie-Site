57d77811a78f0efc7e116671a859d4671a43951b
joykara <joykarani1@gmail.com>
2023-04-05 09:47:06 +0300

    set API link and serch query

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const API_LINK = "https://api.themoviedb.org/3/movie/550?api_key=6e7b2cbc9fa2d51988471aaf765f0237"
+const API_LINK = "https://api.themoviedb.org/3/movie/550?api_key=6e7b2cbc9fa2d51988471aaf765f0237&page=1"
 const imgPath = ""
-const searchAPI = ""
-const
+const searchAPI = "https://api.themoviedb.org/3/movie/550?api_key=6e7b2cbc9fa2d51988471aaf765f0237&query="
