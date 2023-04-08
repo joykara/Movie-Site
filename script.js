396cb02c1f4988bf4da49a2f05a3f8215f34f8a7
joykara <joykarani1@gmail.com>
2023-04-08 08:26:09 +0300

    set up js

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-const API_LINK = "https://api.themoviedb.org/3/movie/550?api_key=6e7b2cbc9fa2d51988471aaf765f0237&page=1"
-const imgPath = ""
-const searchAPI = "https://api.themoviedb.org/3/movie/550?api_key=6e7b2cbc9fa2d51988471aaf765f0237&query="
+const API_LINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1';
+const imgPath = "https://image.tmdb.org/t/p/w1280";
+const searchAPI = "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
 returnMovies(API_LINK)
+
 function returnMovies(url) {
-    fetch(url).then[res => res.json()]
+    fetch(url).then(res => res.json())
         .then(function (data) {
             console.log(data.results);
             data.results.forEach(element => {
                 const div_card = document.createElement("div");
+                div_card.setAttribute("class", "movie-card");
+
                 const div_row = document.createElement("div");
+                div_row.setAttribute("class", "row");
+
                 const div_column = document.createElement("div");
+                div_column.setAttribute("class", "column");
+
                 const image = document.createElement("img");
+                image.setAttribute("class", "thumbnail");
+                image.setAttribute("id", "image");
+
                 const title = document.createElement("h3");
+                title.setAttribute("id", "title");
+
                 const center = document.createElement("center");
 
-                title.innerHTML = $(element.title);
+                title.innerHTML = `${element.title}`;
                 image.src = imgPath + element.poster_path; //returns link as posterpath
 
                 center.appendChild(image);  //add an element under another element
