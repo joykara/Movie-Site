e3fe75fcdf00cb3dac2cd7198329142df67517b5
joykara <joykarani1@gmail.com>
2023-04-08 07:48:44 +0300

    append elements in the section

--- script.js
+++ script.js
@@ -6,12 +6,30 @@ const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
+returnMovies(API_LINK)
 function returnMovies(url) {
     fetch(url).then[res => res.json()]
         .then(function (data) {
             console.log(data.results);
             data.results.forEach(element => {
-                const div = document.createElement("div");
+                const div_card = document.createElement("div");
+                const div_row = document.createElement("div");
+                const div_column = document.createElement("div");
+                const image = document.createElement("img");
+                const title = document.createElement("h3");
+                const center = document.createElement("center");
+
+                title.innerHTML = $(element.title);
+                image.src = imgPath + element.poster_path; //returns link as posterpath
+
+                center.appendChild(image);  //add an element under another element
+                div_row.appendChild(div_column);
+                div_column.appendChild(div_card);
+                div_card.appendChild(center);
+                div_card.appendChild(title);
+
+                main.appendChild(div_row);
+
             });
     })
 }
