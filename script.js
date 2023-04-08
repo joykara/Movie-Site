b0d8b431a03d440c1eed482d27b2f54b6e7b1759
joykara <joykarani1@gmail.com>
2023-04-08 08:43:13 +0300

    linked API to web page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const API_LINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1';
+const API_LINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6e7b2cbc9fa2d51988471aaf765f0237&page=1';
 const imgPath = "https://image.tmdb.org/t/p/w1280";
-const searchAPI = "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+const searchAPI = "https://api.themoviedb.org/3/search/movie?&api_key=6e7b2cbc9fa2d51988471aaf765f0237&query=";
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
@@ -46,7 +46,7 @@ function returnMovies(url) {
     })
 }
 
-form.addEventListener("submit"), (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     main.innerHTML = "";
 
@@ -56,4 +56,4 @@ form.addEventListener("submit"), (e) => {
         returnMovies(searchAPI + searchItem);
         search.value = "";  //clear input value
     }
-}
+});
